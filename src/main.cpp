37c306e02106c3f098b7cd6c9083b71533b26412
umutbide1 <umut.bide.564@gmail.com>
2024-10-09 23:30:31 +0300

    Dosya hiyerarşisi sonrası ilk cpp kod denemesi ve makefile bağlama denemeleri

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world,uzunca bir süre sonra tekrar cpp yazmak özlendi mi özlenmedi mi tam anlamadım ama :)" << endl;
+    
+
+
+
+}
